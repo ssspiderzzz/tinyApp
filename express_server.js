62791ac7362d432dd1ae71b877dcb6d97507b5bf
ssspiderzzz <ssspiderzzz@gmail.com>
2019-07-09 12:45:50 -0700

    w3d1 done

--- express_server.js
+++ express_server.js
@@ -9,6 +9,8 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -23,15 +25,40 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  let shortRandom = generateRandomString();
+  urlDatabase[shortRandom] = req.body.longURL;
+  console.log(req.body);  // Log the POST request body to the console
+  res.redirect(`/urls/${shortRandom}`);
+});
+
 app.get("/hello", (req, res) => {
   let templateVars = { greeting: "Hello World" };
   res.render("hello_world", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+const generateRandomString = function() {
+  let result = "";
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 6; ++i) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+};
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
